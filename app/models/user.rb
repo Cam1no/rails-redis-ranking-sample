755c932dbb0c22651eabd92004b2becca86501ed
joe <kinjoyuji@gmail.com>
2018-07-12 22:49:10 +0900

    Articleモデル追加によるUserモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  has_many :articles, dependent: :destroy
+
   validates :name,  presence: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
